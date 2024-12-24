4fe3370e7e4d1a0c3305e816ea58d8ac943232c7
LOHITH54 <lingutlalohith@gmail.com>
2024-12-24 20:56:41 +0530

    Update BrickBreakerGame.java

--- BrickBreakerGame.java
+++ BrickBreakerGame.java
@@ -46,33 +46,23 @@ class Gameplay extends JPanel implements KeyListener, ActionListener {
     }
 
     public void paint(Graphics g) {
-        // Background
         g.setColor(Color.black);
         g.fillRect(1, 1, 692, 592);
 
-        // Drawing the map
         map.draw((Graphics2D) g);
 
-        // Borders
         g.setColor(Color.yellow);
         g.fillRect(0, 0, 3, 592);
         g.fillRect(0, 0, 692, 3);
         g.fillRect(691, 0, 3, 592);
-
-        // Scores
         g.setColor(Color.white);
         g.setFont(new Font("serif", Font.BOLD, 25));
         g.drawString("Score: " + score, 560, 30);
-
-        // Paddle
         g.setColor(Color.green);
         g.fillRect(playerX, 550, 100, 8);
-
-        // Ball
         g.setColor(Color.yellow);
         g.fillOval(ballPosX, ballPosY, 20, 20);
 
-        // Game Over
         if (ballPosY > 570) {
             play = false;
             ballDirX = 0;
@@ -84,8 +74,6 @@ class Gameplay extends JPanel implements KeyListener, ActionListener {
             g.setFont(new Font("serif", Font.BOLD, 20));
             g.drawString("Press Enter to Restart", 230, 350);
         }
-
-        // Win Condition
         if (totalBricks == 0) {
             play = false;
             ballDirX = 0;
@@ -101,7 +89,6 @@ class Gameplay extends JPanel implements KeyListener, ActionListener {
         g.dispose();
     }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
         timer.start();
 
@@ -150,17 +137,10 @@ class Gameplay extends JPanel implements KeyListener, ActionListener {
                 ballDirX = -ballDirX;
             }
         }
-
         repaint();
     }
-
-    @Override
     public void keyTyped(KeyEvent e) {}
-
-    @Override
     public void keyReleased(KeyEvent e) {}
-
-    @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             if (playerX >= 600) {
@@ -177,7 +157,6 @@ class Gameplay extends JPanel implements KeyListener, ActionListener {
                 moveLeft();
             }
         }
-
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             if (!play) {
                 play = true;
@@ -189,7 +168,6 @@ class Gameplay extends JPanel implements KeyListener, ActionListener {
                 score = 0;
                 totalBricks = 21;
                 map = new MapGenerator(3, 7);
-
                 repaint();
             }
         }
